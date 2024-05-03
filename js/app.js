6936c9ed4dc5b7003ef894184900c1f1048563ef
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-05-03 11:54:39 -0400

    boton funcional 3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-// var h1Element = document.querySelector('.lbltitulo');
-// h1Element.innerText = h1Element.innerText + ' tu casa';
+
+function btnResp(resp) {
+    var h1Element = document.querySelector('.lbltitulo');
+    if (resp === 'S') {
+        h1Element.innerText = ' Muy Bien';
+    }
+    else {
+        h1Element.innerText = h1Element.innerText + ', Seguro?';
+    }
+}
+
+
 document.addEventListener('keydown', function () {
     pumImg();
 });
