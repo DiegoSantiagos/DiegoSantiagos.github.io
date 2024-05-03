4f8cc8ffb7f8474483aba9a0cfca16819f8e0369
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-05-03 11:29:17 -0400

    funcion al boton

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-var h1Element = document.querySelector('.lbltitulo');
-h1Element.innerText = h1Element.innerText + ' tu casa';
+// var h1Element = document.querySelector('.lbltitulo');
+// h1Element.innerText = h1Element.innerText + ' tu casa';
 
 function hideElement() {
     main.style.display = 'none';
+}
+
+function pumImg() {
+    var imgElement = document.querySelector('.pumImg');
+    imgElement.style.display = 'block';
+
+    setTimeout(function () {
+        imgElement.style.display = 'none';
+    }, 1000)
 }
